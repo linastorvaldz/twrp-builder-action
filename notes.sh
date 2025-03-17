52574fdcaf3012be4c4a9d7fa465ca2de0b04251
hazepynut <naden.hey@gmail.com>
2025-03-17 09:54:17 +0800

    treewide: major changes
    Signed-off-by: hazepynut <naden.hey@gmail.com>

--- notes.sh
+++ notes.sh
@@ -5,10 +5,10 @@ echo "## OrangeFox Recovery Build - Unofficial
 📱 Device: ${DEVICE} | ${CODENAME}
 📂 ZIP Size: ${ORF_ZIP_SIZE}
 📂 Image Size: ${ORF_IMG_SIZE}
-👩‍💻 Top Commit: \`${DT_COMMIT}\`
+👩‍💻 Top Commit: 
+\`${DT_COMMIT}\`
 
 📕 ZIP MD5: \`${ORF_ZIP_MD5}\`
 📘 ZIP SHA1: \`${ORF_ZIP_SHA1}\`
 📕 Image MD5: \`${ORF_IMG_MD5}\`
-📘 Image SHA1: \`${ORF_IMG_SHA1}\`" \
->> ${GITHUB_ACTION_PATH}/release-notes.md
+📘 Image SHA1: \`${ORF_IMG_SHA1}\`" >>${GITHUB_ACTION_PATH}/release-notes.md
